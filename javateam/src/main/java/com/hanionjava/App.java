55cc622c40f4a982524c1a01f6a39bbb880e9c22
unknown <hanishasalian@gmail.com>
2025-06-04 19:00:29 +0530

    azure pipeline example1

---
+++ javateam/src/main/java/com/hanionjava/App.java
@@ -0,0 +1,13 @@
+package com.hanionjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
